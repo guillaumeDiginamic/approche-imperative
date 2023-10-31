c1f8ae498c1e57d466d3ef0b5ee97df0b600ce98
guillaumeDiginamic <cguillaume@diginamic-formation.fr>
2023-10-31 17:51:21 +0100

    modification classes

--- src/fr/algorithmie/FirstLast.java
+++ src/fr/algorithmie/FirstLast.java
@@ -5,18 +5,17 @@ public class FirstLast {
 
         //int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4} ;
         //int[] array1 = {};
-        //int[] array1 = {6, 15, -3, 8, 7, 4, -2, 28};
-        int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, 6} ;
+        int[] array1 = {4, 15, -3, 8, 7, 4, -2, 4};
         int size=array1.length;
         int count = 0;
         boolean bool = false;
 
         // calculer une valeur booléenne qui contrôle le tableau de la sorte
-        // elle vaut true si le tableau a au moins 1 élément et si le premier élément ou le dernier élément vaut 6.
+        // elle vaut true si le tableau est de longueur supérieure ou égale à 1 et que le premier et le dernier élément du tableau ont la même valeur
         // elle vaut false dans les autres cas
 
         if (size > 0) {
-            if ((array1[0]==6) || (array1[size-1]==6)) {
+            if (array1[0]==array1[size-1]) {
                 bool =true;
             }
         }
--- src/fr/algorithmie/FirstLast6.java
+++ src/fr/algorithmie/FirstLast6.java
@@ -3,10 +3,17 @@ package fr.algorithmie;
 public class FirstLast6 {
     public static void main(String[] args) {
 
-        int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4} ;
+        //int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4} ;
+        //int[] array1 = {};
+        //int[] array1 = {6, 15, -3, 8, 7, 4, -2, 28};
+        int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, 6} ;
         int size=array1.length;
         int count = 0;
         boolean bool = false;
+
+        // calculer une valeur booléenne qui contrôle le tableau de la sorte
+        // elle vaut true si le tableau a au moins 1 élément et si le premier élément ou le dernier élément vaut 6.
+        // elle vaut false dans les autres cas
 
         if (size > 0) {
             if ((array1[0]==6) || (array1[size-1]==6)) {
@@ -14,8 +21,6 @@ public class FirstLast6 {
             }
         }
         System.out.println("condition : "+bool);
-
-
     }
 
 }
